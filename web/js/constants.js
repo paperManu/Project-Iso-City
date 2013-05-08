6872e3afa5761924e348da69f6fde2200bd353cc
Emmanuel Durand <emmanueldurand@gmail.com>
2013-05-08 08:38:50 -0400

    Picking !

--- web/js/constants.js
+++ web/js/constants.js
@@ -2,5 +2,9 @@
 //
 // Some useful constants and definitions
 
+// Geometry constants
 var cBaseBlocSize = 16;
 var cBaseElementSize = 4;
+
+// Camera constants
+var cFOV = 50; // Half of the orthographic "FOV"
--- web/js/second.js
+++ web/js/second.js
@@ -2,6 +2,7 @@ function cos(x) {return Math.cos(x);}
 function sin(x) {return Math.sin(x);}
 
 var _renderer, _scene, _camera;
+var _height, _width;
 
 /*************/
 function createScene() {
@@ -29,12 +30,21 @@ function createScene() {
 var camPosition = new THREE.Vector3(2, 1.5, 2);
 var newCamPosition, newCamDirection;
 function init() {
+    _renderer = new THREE.WebGLRenderer();
+    _renderer.setSize(window.innerWidth, window.innerHeight);
+
+    document.body.appendChild(_renderer.domElement);
+
+    _width = _renderer.domElement.width;
+    _height = _renderer.domElement.height
+
     _scene = new THREE.Scene();
 
     camDirection = new THREE.Vector3(16, 0, 16);
 
     var ratio = window.innerWidth / window.innerHeight;
-    _camera = new THREE.OrthographicCamera(-50, 50, 50/ratio, -50/ratio, 1, 1000);
+    _camera = new THREE.OrthographicCamera(-cFOV, cFOV, cFOV/ratio, -cFOV/ratio, 1, 1000);
+    //_camera = new THREE.PerspectiveCamera(45, _width/_height, 1, 1000);
     _camera.position = camPosition.setLength(100);
     _camera.lookAt(camDirection);
 
@@ -50,11 +60,34 @@ function init() {
     pointLight.position.z = 500;
     _scene.add(ambientLight);
     _scene.add(pointLight);
+}
 
-    _renderer = new THREE.WebGLRenderer();
-    _renderer.setSize(window.innerWidth, window.innerHeight);
-
-    document.body.appendChild(_renderer.domElement);
+/*************/
+window.onmousedown = function(ev) {
+    if (ev.target == _renderer.domElement) {
+        var projector = new THREE.Projector();
+        sx = ev.clientX;
+        sy = ev.clientY;
+        //var v = new THREE.Vector3((sx / _width) * 2 - 1, -(sy / _height) * 2 + 1, 0.5);
+        //projector.unprojectVector(v, _camera);
+        var v = new THREE.Vector3(((sx / _width) * 2 - 1) * cFOV,
+                                 (-(sy / _height) * 2 + 1) * cFOV,
+                                  0.0);
+        var rotMat = new THREE.Matrix4();
+        rotMat.extractRotation(_camera.matrixWorld);
+        v = v.applyMatrix4(rotMat);
+        var n = new THREE.Vector3(0, 0, -1);
+        n = n.applyMatrix4(rotMat);
+        var position = v.clone();
+        position.add(_camera.position);
+        var rayCaster = new THREE.Raycaster(position, n.normalize(), 10, 1000);
+        var intersects = rayCaster.intersectObject(_scene, true);
+        if (intersects.length > 0) {
+            var obj = _scene.getObjectById(intersects[0].object.id, true);
+            var father = obj.parent;
+            father.parent.remove(father);
+        }
+    }
 }
 
 /*************/
@@ -65,5 +98,4 @@ function draw() {
 }
 
 init();
-grid = _scene.getChildByName("grid", false);
 draw();
